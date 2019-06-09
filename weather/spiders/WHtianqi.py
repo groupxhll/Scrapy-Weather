912d2b18e095a409d1cfd84c99cb10e68a80cff2
groupxhll <51309506+groupxhll@users.noreply.github.com>
2019-06-09 22:29:51 +0800

    Update WHtianqi.py

--- weather/spiders/WHtianqi.py
+++ weather/spiders/WHtianqi.py
@@ -11,18 +11,17 @@ class WhtianqiSpider(scrapy.Spider):
         items = []
         sixday = response.xpath('/html/body/div[5]/div/div[2]/div[2]')
         for i in range(0,6):
-            for day in sixday:
-                item = WeatherItem()
-                date = day.xpath('/html/body/div[5]/div/div[2]/div[2]/ul[1]/li/b/text()')[i].extract()
+            item = WeatherItem()
+            date = sixday.xpath('/html/body/div[5]/div/div[2]/div[2]/ul[1]/li/b/text()')[i].extract()
 
             item['date'] = date
 
-            item['week'] = day.xpath('/html/body/div[5]/div/div[2]/div[2]/ul[1]/li/span/text()')[i].extract()
-            item['img'] = day.xpath('/html/body/div[5]/div/div[2]/div[2]/ul[1]/li/img/@src')[i].extract()
-            item['hightemperature'] = day.xpath('/html/body/div[5]/div/div[2]/div[2]/div/ul/li/span/text()')[i].extract()
-            item['lowtemperature'] = day.xpath('/html/body/div[5]/div/div[2]/div[2]/div/ul/li/b/text()')[i].extract()
-            item['weather'] = day.xpath('/html/body/div[5]/div/div[2]/div[2]/ul[2]/li/text()')[i].extract()
-            item['wind'] = day.xpath('/html/body/div[5]/div/div[2]/div[2]/ul[3]/li/text()')[i].extract()
+            item['week'] = sixday.xpath('/html/body/div[5]/div/div[2]/div[2]/ul[1]/li/span/text()')[i].extract()
+            item['img'] = sixday.xpath('/html/body/div[5]/div/div[2]/div[2]/ul[1]/li/img/@src')[i].extract()
+            item['hightemperature'] = sixday.xpath('/html/body/div[5]/div/div[2]/div[2]/div/ul/li/span/text()')[i].extract()
+            item['lowtemperature'] = sixday.xpath('/html/body/div[5]/div/div[2]/div[2]/div/ul/li/b/text()')[i].extract()
+            item['weather'] = sixday.xpath('/html/body/div[5]/div/div[2]/div[2]/ul[2]/li/text()')[i].extract()
+            item['wind'] = sixday.xpath('/html/body/div[5]/div/div[2]/div[2]/ul[3]/li/text()')[i].extract()
             items.append(item)
 
         return items
